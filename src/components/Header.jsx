24facd642baa9032937568a40708c9d417bb593a
Ngarukiyimana Sostene <ngarukiyimanasostene@gmail.com>
2023-05-02 11:34:11 +0200

    fix(signup): fixing signup feature
    - implement fixing redux initial state
    - implement props desctructuring
    
    [Finished fix-signup ]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,15 @@
+/* eslint-disable react/no-array-index-key */
 import { Link } from 'react-router-dom';
+import { navLinks } from '../constants/navLinks';
 
 const Header = () => {
 	return (
 		<div className="link">
-			<Link to="/" className="px-4">
-				Home
-			</Link>
-			<Link to="/Login" className="px-4">
-				Login
-			</Link>
-			<Link to="/buyer" className="px-4">
-				Buyer
-			</Link>
-			<Link to="/vendor" className="px-4">
-				Vendor
-			</Link>
+			{navLinks.map((link, index) => (
+				<Link key={index} to={link.path} className="px-4">
+					{link.name}
+				</Link>
+			))}
 		</div>
 	);
 };
---
+++ src/constants/navLinks.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'Login',
+		path: '/login',
+	},
+	{
+		name: 'vendor',
+		path: '/Vendor',
+	},
+	{
+		name: 'Buyer',
+		path: '/buyer',
+	},
+];
