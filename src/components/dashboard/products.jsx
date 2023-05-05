ec71dcaa85a6a2ec67b8c48187db393efc05c09c
chrill-christella <ufiteyezuchristella@gmail.com>
2023-05-05 07:46:07 +0200

    ft-seller-view-product implement seller component implement integration to get products implement view a specific item by seller [finishes]

--- src/components/dashboard/products.jsx
+++ src/components/dashboard/products.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
 import { MdEdit, MdOutlineDeleteOutline } from 'react-icons/md';
 import Button from '../Button/Button';
 
 function Products() {
+	const [products, setProducts] = useState([]);
+
+	const handleAddProduct = () => {
+		const newProduct = {
+			name: 'New Product',
+			price: '$100',
+		};
+		setProducts([...products, newProduct]);
+	};
+
 	return (
 		<div className="pt-40 pl-32 w-3/4 ">
 			<div className=" bg-brightGray  border-0 rounded-xl py-4 mx-14  pl-5 pr-5">
 				<div className="  flex justify-between  pr-20">
-					<h1 className="py-4  font-bold fontSize-100px">All Products</h1>
+					<h1 className="py-4 font-bold text-4xl">All Products</h1>
 					<Button
-						label="Add Products"
+						label="Add Product"
 						className="bg-primaryGreen text-white font-bold w-40 rounded-full p-1 my-2 flex justify-center items-center"
+						onClick={handleAddProduct}
 					/>
 				</div>
 				<div className="">
@@ -18,43 +30,22 @@ function Products() {
 						<tbody className="">
 							<tr className="p-10 font-bold text-2xl pl-10 pr-20 h-14">
 								<td className="text-2xl">Product</td>
-								<td> price</td>
-								<td> </td>
-								<td> </td>
+								<td>Price</td>
+								<td>Edit</td>
+								<td>Delete</td>
 							</tr>
-							<tr className="h-14">
-								<td>Iphone</td>
-								<td> $1200</td>
-								<td>
-									<MdEdit />
-								</td>
-								<td>
-									{' '}
-									<MdOutlineDeleteOutline />
-								</td>
-							</tr>
-							<tr className="h-14">
-								<td>Iphone</td>
-								<td> $1200</td>
-								<td>
-									<MdEdit />
-								</td>
-								<td>
-									{' '}
-									<MdOutlineDeleteOutline />
-								</td>
-							</tr>
-							<tr className="h-14">
-								<td>Iphone</td>
-								<td> $1200</td>
-								<td>
-									<MdEdit />
-								</td>
-								<td>
-									{' '}
-									<MdOutlineDeleteOutline />
-								</td>
-							</tr>
+							{products.map((product, index) => (
+								<tr className="h-14" key={index}>
+									<td>{product.name}</td>
+									<td>{product.price}</td>
+									<td>
+										<MdEdit /> Edit
+									</td>
+									<td>
+										<MdOutlineDeleteOutline /> Delete
+									</td>
+								</tr>
+							))}
 						</tbody>
 					</table>
 				</div>
